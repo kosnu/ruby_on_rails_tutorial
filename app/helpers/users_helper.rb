399dc5f072608c29390b6bfc1248712b468aee08
kosnu <coursek8814@gmail.com>
2019-06-18 01:11:59 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    garavatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(garavatar_url, alt: user.name, class: 'garatar')
+  end
 end

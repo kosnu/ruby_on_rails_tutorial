674cbc34eb86deef22a8141ce0824568406d6d79
kosnu <coursek8814@gmail.com>
2019-06-27 03:33:43 +0900

    Add Login process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Redirect user info after user login
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end

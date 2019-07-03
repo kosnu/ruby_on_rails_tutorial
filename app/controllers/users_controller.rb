5f88ce23a8cf7affc57e504c89d43ab18e48f5ad
kosnu <coursek8814@gmail.com>
2019-07-04 03:23:21 +0900

    Add users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: %i[index edit update]
+  before_action :correct_user, only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -41,8 +47,18 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
